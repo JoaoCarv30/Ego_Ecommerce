f6d3cc7540a5915aa117b21e68088fe393c62db9
Joao Victor <Carvalho.joao1@gmail.com>
2024-10-23 13:49:21 -0300

    Cart Button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import logo from '../../assets/Zahrilogo.png'
+import { IoCart } from "react-icons/io5";
+
 
 const Header = () => {
   return (
@@ -8,12 +10,17 @@ const Header = () => {
  
       <img src={logo} alt="#" className='w-1/12' />
 
-      <nav>
+      <nav className='flex items-center justify-center gap-8'>
         <ul className='flex items-center justify-center gap-8 decoration-transparent text-xl font-bold'>
           <li>Home</li>
           <li>Produtos</li>
           <li>Contato</li>
         </ul>
+        <div className='relative '>
+          <IoCart className='text-3xl' />
+          <span className='absolute -top-1 -right-1 bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full'>0</span>
+
+        </div>
       </nav>
     </header>
   )
