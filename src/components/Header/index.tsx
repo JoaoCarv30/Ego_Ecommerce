4eb23dfa7fc67510093c345c30f4c2e523520447
João Carvalho <Carvalhoestudante1@gmail.com>
2025-02-12 10:08:22 -0300

    padding and ico

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,7 +19,7 @@ const Header = () => {
 
       <nav className='flex items-center justify-center gap-8'>
        
-        <Link to='/cart'>
+        <Link to='/cart' className='p-4'>
           <div className='relative '>
             <IoCart className='text-3xl' />
             <span className='absolute -top-1 -right-1 bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full'>{cartQuantity}</span>
